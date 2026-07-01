79d18a794bccd93c80627a85e90529ca2ef952a9
Aviral Vishwakarma <aviralvishwakarma456@gmail.com>
2026-07-01 10:04:01 +0530

    Time: 46 ms (18.34%) | Memory: 59.2 MB (45.62%) - LeetSync

--- Arrays/15-3sum/3sum.java
+++ Arrays/15-3sum/3sum.java
@@ -1,20 +1,39 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        Set<List<Integer>> list = new HashSet<>();
-        int n = nums.length;
-        for(int i = 0 ; i < n ; i++){
-            Set<Integer> set = new HashSet<>();
-            for(int j = i+2 ; j < n ; j++){
-                set.add(nums[j-1]);
-                int t =  -(nums[i]+nums[j]);
-                if(set.contains(t)){
-                    List<Integer> temp = Arrays.asList(nums[i] , nums[j] , t);
-                    Collections.sort(temp);
-                    list.add(temp);
+        Arrays.sort(nums);
+        List<List<Integer>> list = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) { //Duplicate value of i
+                continue;
+            }
+            int j = i + 1;
+            int k = nums.length - 1;
+            while (j < k) {
+                if (nums[i] + nums[j] + nums[k] == 0) {
+                    List<Integer> triplet = new ArrayList(Arrays.asList(nums[i], nums[j], nums[k]));
+                    list.add(triplet);
+                    int temp_j = nums[j];
+                    int temp_k = nums[k];
+                    while (j < nums.length && nums[j] == temp_j) {
+                        j++;
+                    }
+                    while (k > 0 && nums[k] == temp_k) {
+                        k--;
+                    }
+                } else if (nums[i] + nums[j] + nums[k] < 0) {
+                    int temp_j = nums[j];
+                    while (j < nums.length && nums[j] == temp_j) {
+                        j++;
+                    }
+                } else {
+                    int temp_k = nums[k];
+                    while ( k >0  && nums[k] == temp_k) {
+                        k--;
+                    }
                 }
             }
         }
-        List<List<Integer>> triplet = new ArrayList<>(list);
-        return triplet;
+        return list;
+
     }
 }
