4959034f692f07926a09cc44cf04bab9a8de06b6
Aviral Vishwakarma <aviralvishwakarma456@gmail.com>
2026-07-01 11:19:16 +0530

    Time: 506 ms (5.05%) | Memory: 47.4 MB (7.33%) - LeetSync

---
+++ Arrays/18-4sum/4sum.java
@@ -0,0 +1,27 @@
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        int n = nums.length;
+        Set<List<Integer>> set = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                Set<Integer> quad = new HashSet<>();
+                for (int k = j + 2; k < n; k++) {
+                    long t = (long) target -nums[i] - nums[j] - nums[k];
+                    quad.add(nums[k - 1]);
+                    if (t >= Integer.MIN_VALUE &&
+                            t <= Integer.MAX_VALUE && //Confusion 
+                            quad.contains((int) t)) {
+
+                        List<Integer> list = new ArrayList<>(
+                                Arrays.asList(nums[i], nums[j], nums[k], (int) t));
+                        Collections.sort(list);
+                        set.add(list);
+                    }
+
+                }
+            }
+        }
+        List<List<Integer>> all = new ArrayList<>(set);
+        return all;
+    }
+}
