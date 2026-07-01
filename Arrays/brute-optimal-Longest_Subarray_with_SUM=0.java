beb09467fc740b38ca4d12682cdb5a571cbb4e0c
Aviral Vishwakarma <aviralvishwakarma456@gmail.com>
2026-07-01 16:20:32 +0530

    Update brute-optimal-Longest_Subarray_with_SUM=0.java

--- Arrays/brute-optimal-Longest_Subarray_with_SUM=0.java
+++ Arrays/brute-optimal-Longest_Subarray_with_SUM=0.java
@@ -1,3 +1,4 @@
+//Brute Force
 class Solution {
     int maxLength(int arr[]) {
         int n = arr.length;
@@ -8,6 +9,30 @@ class Solution {
                 sum+=arr[j];
                 if(sum==0){
                     max = (max>((j-i)+1))?max:(j-i)+1;
+                }
+            }
+        }
+        return max;
+    }
+}
+
+//optimal
+class Solution {
+    int maxLength(int arr[]) {
+        int sum=0;
+        int max=0;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i = 0 ; i < arr.length ; i++){
+            sum+=arr[i];
+            if(sum==0){
+                max=i+1;
+            }
+            else{
+                if(map.containsKey(sum)){
+                    max=Math.max(max,i-map.get(sum));
+                }
+                else{
+                    map.put(sum,i);
                 }
             }
         }
