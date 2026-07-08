0b06a66892b72ef0a7d14d458520f7bc336b44ec
Aviral Vishwakarma <aviralvishwakarma456@gmail.com>
2026-07-08 13:40:01 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (24.33%) - LeetSync

--- BS/74-search-a-2d-matrix/search-a-2d-matrix.java
+++ BS/74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -3,6 +3,33 @@ class Solution {
         int n = nums.length;
         int m = nums[0].length;
         int l = 0 ;
+        int r = n*m-1;
+        while(l<=r){
+            int mid = (l+r)/2;
+            int i = mid/m;
+            int j = mid%m;
+            if(nums[i][j]==target){
+                return true;
+            }
+            else if(nums[i][j]<target){
+                l = mid +1 ;
+            }
+            else{
+                r = mid - 1;
+            }
+        } 
+        return false;
+    }
+}
+
+
+//TC O(m+n)
+/*
+class Solution {
+    public boolean searchMatrix(int[][] nums, int target) {
+        int n = nums.length;
+        int m = nums[0].length;
+        int l = 0 ;
         int r = n*m;
         for(l=0;l<r;l++){
             int i = l/m ;
@@ -14,3 +41,4 @@ class Solution {
         return false;
     }
 }
+ */
