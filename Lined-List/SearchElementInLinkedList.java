f6c45c3a31b15561d05722dfb783788320f05ae1
Aviral Vishwakarma <aviralvishwakarma456@gmail.com>
2026-07-10 14:34:17 +0530

    Implement searchKey method in linked list

---
+++ Lined-List/SearchElementInLinkedList.java
@@ -0,0 +1,20 @@
+/*
+  class Node {
+   int data;
+    Node next;
+
+    Node(int d)  { data = d;  next = null; }
+}
+*/
+class Solution {
+    public boolean searchKey(Node head, int key) {
+        Node current = head;
+        while(current!=null){
+            if(current.data==key){
+                return true;
+            }
+            current = current.next;
+        }
+        return false;
+    }
+}
