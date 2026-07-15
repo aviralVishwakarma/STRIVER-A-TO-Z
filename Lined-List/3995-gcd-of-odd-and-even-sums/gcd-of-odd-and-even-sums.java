07dcd05886ee698acf31e3b14f66e7b88e08d2c9
Aviral Vishwakarma <aviralvishwakarma456@gmail.com>
2026-07-15 10:58:00 +0530

    Time: 1 ms (83.11%) | Memory: 42.5 MB (74.78%) - LeetSync

--- Lined-List/3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.java
+++ Lined-List/3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.java
@@ -3,3 +3,20 @@ class Solution {
         return n;
     }
 }
+/**
+
+class Solution {
+    public int gcdOfOddEvenSums(int n) {
+        int odd = n*n;
+        int even = (n)*(n+1);
+        int min = Math.min(odd,even);
+        while(even!=0){
+            int rem = odd%even;
+            odd=even;
+            even=rem;
+
+        }
+        return odd;
+    }
+}
+ */
