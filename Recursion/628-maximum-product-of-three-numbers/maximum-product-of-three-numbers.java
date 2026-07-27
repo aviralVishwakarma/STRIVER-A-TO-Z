bec985db01ae48003f40c54fa32ae93e6a239447
Aviral Vishwakarma <aviralvishwakarma456@gmail.com>
2026-07-27 20:21:24 +0530

    Time: 3 ms (86.49%) | Memory: 47.1 MB (93.88%) - LeetSync

--- Recursion/628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.java
+++ Recursion/628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.java
@@ -1,8 +1,32 @@
 class Solution {
     public int maximumProduct(int[] nums) {
-        Arrays.sort(nums);
-        int n = nums[nums.length-1]*nums[nums.length-2]*nums[nums.length-3];
-        int m = nums[0]*nums[1]*nums[nums.length-1];
-        return Math.max(n,m);
+        int neg_one = Integer.MAX_VALUE;
+        int neg_two = neg_one;
+        int pos_one = Integer.MIN_VALUE;
+        int pos_two = pos_one;
+        int pos_three = pos_two;
+        for(int i = 0 ; i < nums.length ; i++){
+                if(nums[i]>=pos_one){
+                    pos_three = pos_two;
+                    pos_two = pos_one;
+                    pos_one = nums[i];
+                }
+                else if (nums[i]>pos_two){
+                    pos_three=pos_two;
+                    pos_two = nums[i];
+                }
+                else if(nums[i]>pos_three){
+                    pos_three = nums[i];
+                }
+                if(nums[i]<=neg_one){
+                    neg_two = neg_one;
+                    neg_one = nums[i];
+                }
+                else if(neg_two>nums[i]){
+                    neg_two = nums[i];
+                    
+                }
+        }
+        return Math.max(pos_one*pos_two*pos_three,neg_one*neg_two*pos_one);
     }
 }
