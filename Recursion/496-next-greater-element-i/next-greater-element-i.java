8cbf78a70a2833e98d38a08ff5369237b2f06643
Aviral Vishwakarma <aviralvishwakarma456@gmail.com>
2026-08-12 16:52:52 +0530

    Time: 4 ms (74.23%) | Memory: 45.4 MB (22.51%) - LeetSync

---
+++ Recursion/496-next-greater-element-i/next-greater-element-i.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        Stack<Integer> stack = new Stack<>();
+        for(int i = nums2.length-1 ; i > -1 ; i--){
+            if(stack.isEmpty()){
+                map.put(nums2[i],-1);
+            }
+            else if(stack.peek()>=nums2[i]){
+                map.put(nums2[i],stack.peek());
+            }
+            else{
+                while(!stack.isEmpty() && stack.peek()<nums2[i] ){
+                    stack.pop();
+                }
+                if(stack.isEmpty()){
+                    map.put(nums2[i],-1);
+                }
+                else{
+                    map.put(nums2[i],stack.peek());
+                }
+            }
+            stack.push(nums2[i]);
+        }
+        int[] arr = new int[nums1.length];
+        for(int i = 0 ; i <nums1.length ; i++){
+            arr[i] = map.get(nums1[i]);
+        }
+        return arr;
+    }
+}
