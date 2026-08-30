b01589f249dbab65836fc3ea5017cb439f5a45c2
Aviral Vishwakarma <aviralvishwakarma456@gmail.com>
2026-08-31 00:07:25 +0530

    Time: 3 ms (9.44%) | Memory: 72.4 MB (49.19%) - LeetSync

--- Recursion/890-lemonade-change/lemonade-change.java
+++ Recursion/890-lemonade-change/lemonade-change.java
@@ -1,41 +1,24 @@
 class Solution {
     public boolean lemonadeChange(int[] bills) {
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for(int i = 0 ; i < bills.length ; i++){
-            map.put(bills[i],map.getOrDefault(bills[i],0)+1);
-            int rem = bills[i]-5;
-            if(rem==0) continue;
-            else if(rem==5){
-                if(map.containsKey(5)){
-                    map.put(5,map.get(5)-1);
-                    if(map.get(5)==0){
-                        map.remove(5);
-                    }
-                }
-                else{
-                    return false;
-                }
-            }
-            else{
-                if(map.containsKey(5) && map.containsKey(10)){
-                    map.put(5,map.get(5)-1);
-                    if(map.get(5)==0){
-                        map.remove(5);
-                    }
-                    map.put(10,map.get(10)-1);
-                    if(map.get(10)==0){
-                        map.remove(10);
-                    }
-                }
-                else if(map.containsKey(5) && map.get(5)>=3){
-                    map.put(5,map.get(5)-3);
-                    if(map.get(5)==0){
-                        map.remove(5);
-                    }
-                }
-                else{
-                    return false;
-                }
+        int five = 0;
+        int ten = 0;
+        for (int i = 0; i < bills.length; i++) {
+            if (bills[i] == 5)
+                five++;
+            else if (bills[i] == 10)
+                ten++;
+            int rem = bills[i] - 5;
+            if (rem == 0)
+                continue;
+            else if (rem == 5 && five >= 1) {
+                five--;
+            } else if (ten >= 1 && five >= 1) {
+                five--;
+                ten--;
+            } else if (five >= 3) {
+                five -= 3;
+            } else {
+                return false;
             }
         }
         return true;
