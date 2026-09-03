1d1fc8e7b719ab099952818cbbe0782d6b44527a
Aviral Vishwakarma <aviralvishwakarma456@gmail.com>
2026-09-03 18:21:06 +0530

    Time: 5 ms (83.33%) | Memory: 121.6 MB (62.32%) - LeetSync

--- Recursion/4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.java
+++ Recursion/4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.java
@@ -5,8 +5,7 @@ class Solution {
         int odd = 0 ;
         for(int i = 0 ; i < nums1.length ; i++){
             min = Math.min(min,nums1[i]);
-            if(nums1[i]%2==0) even++;
-            else odd++;
+            if(nums1[i]%2!=0) odd++;
         }
         if(min%2!=0) return true;
         else if(odd==0) return true;
