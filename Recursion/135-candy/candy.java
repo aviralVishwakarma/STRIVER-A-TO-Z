c3f17377290398f6049e6f1c57ec018db58e0da7
Aviral Vishwakarma <aviralvishwakarma456@gmail.com>
2026-09-05 12:32:36 +0530

    Time: 3 ms (86.58%) | Memory: 52.5 MB (11.35%) - LeetSync

---
+++ Recursion/135-candy/candy.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int n = ratings.length;
+        int[] candy = new int[n];
+        Arrays.fill(candy,1);
+        //left -- > right
+        for(int i = 0 ; i < n - 1; i++){
+            if(ratings[i]<ratings[i+1]){
+                candy[i+1] = candy[i] + 1;
+            }
+        }
+        //right ---> left
+        for(int i = n - 2 ; i >=0 ; i--){
+            if(ratings[i]>ratings[i+1]){
+                candy[i] = Math.max(candy[i], candy[i + 1] + 1);;
+            }
+        }
+        int ans = 0; 
+        for(int num : candy){
+            ans+=num;
+        }
+        return ans;
+    }
+}
