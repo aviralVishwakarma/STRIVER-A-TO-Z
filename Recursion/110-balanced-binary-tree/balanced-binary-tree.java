a4c540b9e9ca7c775939fa1118b5caf87fa6dee8
Aviral Vishwakarma <aviralvishwakarma456@gmail.com>
2026-09-15 22:49:42 +0530

    Time: 0 ms (100.00%) | Memory: 45.6 MB (41.82%) - LeetSync

--- Recursion/110-balanced-binary-tree/balanced-binary-tree.java
+++ Recursion/110-balanced-binary-tree/balanced-binary-tree.java
@@ -14,31 +14,16 @@
  * }
  */
 class Solution {
-    boolean bool = true;
     public boolean isBalanced(TreeNode root) {
-        traverse(root);
-        return bool;
+        return height(root)!=-1;
     }
-    public void traverse(TreeNode node){
-        if(node==null) return ;
-        max =Integer.MIN_VALUE;
-        depth(node.left,0);
-        int left = max ;
-        max =Integer.MIN_VALUE;
-        depth(node.right,0);
-        int right = max;
-        if(Math.abs(left-right)>1) bool = false ;
-        traverse(node.left);
-        traverse(node.right);
-    }
-    int max =Integer.MIN_VALUE;
-    public void depth(TreeNode node , int n ){
-        if(node==null){
-            max = Math.max(max,n);
-            return;
-        }
-        n++;
-        depth(node.left,n);
-        depth(node.right,n);
+    public int height(TreeNode node){
+        if(node==null) return 0;
+        int left = height(node.left);
+        if(left==-1) return -1 ;
+        int right = height(node.right);
+        if(right==-1) return -1 ;
+        if(Math.abs(left-right)>1) return -1 ;
+        return Math.max(left,right)+1;
     }
 }
